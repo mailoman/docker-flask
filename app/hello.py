9dc2cffafc7d78463d197b48d21a2cd76545bd93
Alexander Milyutin <alexander.milyutin@lazada.com>
2016-02-02 21:53:09 +0700

    minor changes

--- app/hello.py
+++ app/hello.py
@@ -1,5 +1,6 @@
 #!flask/bin/python
-from flask import render_template
+from flask import render_template, make_response
+from flask import request
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,6 +8,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return render_template('hello.html')
+
+@app.route('/install/<vmname>')
+def install(vmname):
+    r = make_response(render_template('install.sh', vmname=vmname), 200)
+    return r
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
